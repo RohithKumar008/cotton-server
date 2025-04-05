d85bac44ef5b98d47f2745e6804ec5a75960c04a
RohithKumar <148546776+RohithKumar008@users.noreply.github.com>
2025-04-05 17:35:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ cursor.execute('''
         id SERIAL PRIMARY KEY,
         image BYTEA,
         timestamp TIMESTAMP,
-        prediction TEXT
+        prediction TEXT,
+        confidence REAL
     )
 ''')
 conn.commit()
@@ -31,10 +32,10 @@ model.eval()
 
 # Define your image transformation
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-])
-
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 @app.route('/')
 def home():
     return "Welcome to the Cotton Disease Classification API!"
@@ -44,26 +45,31 @@ def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
 
-    file = request.files['image']
-    image_bytes = file.read()
-    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-    img_tensor = transform(image).unsqueeze(0)
-
-    with torch.no_grad():
-        output = model(img_tensor)
+    try:
+        file = request.files['image']
+        image_bytes = file.read()
+        image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+        img_tensor = transform(image).unsqueeze(0)
+    
+        with torch.no_grad():
+            output = model(img_tensor)
+        probabilities = torch.softmax(output, dim=1)[0]
         predicted_class = torch.argmax(output, dim=1).item()
+        confidence = probabilities[predicted_class].item()
+    
+        result = f"Predicted class: {predicted_class}"
+        timestamp = datetime.utcnow()
+    
+        # Save image, timestamp and result to PostgreSQL
+        cursor.execute(
+            "INSERT INTO image_results (image, timestamp, prediction,confidence) VALUES (%s, %s, %s, %s)",
+            (psycopg2.Binary(image_bytes), timestamp, result,confidence)
+        )
+        conn.commit()
+    
+        return jsonify({'prediction': result, 'timestamp': str(timestamp), 'confidence':confidence })
 
-    result = f"Predicted class: {predicted_class}"
-    timestamp = datetime.utcnow()
-
-    # Save image, timestamp and result to PostgreSQL
-    cursor.execute(
-        "INSERT INTO image_results (image, timestamp, prediction) VALUES (%s, %s, %s)",
-        (psycopg2.Binary(image_bytes), timestamp, result)
-    )
-    conn.commit()
-
-    return jsonify({'prediction': result, 'timestamp': str(timestamp)})
-
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
     app.run(debug=True)

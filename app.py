f29310a0301737fd47ce89a03ee7400a57cc9db5
RohithKumar <148546776+RohithKumar008@users.noreply.github.com>
2025-04-05 18:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,16 +26,73 @@ cursor.execute('''
 ''')
 conn.commit()
 
-# Load your model
-model = torch.load('best_model_large.pth', map_location=torch.device('cpu'))
-model.eval()
-
 # Define your image transformation
 transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
+
+# --- PyTorch Model Setup ---
+class CNNmodel(nn.Module):
+  def __init__(self,classes):
+    super().__init__()
+
+    self.conv1=nn.Conv2d(3,32,kernel_size=4,stride=1,padding=0)
+    self.bn1=nn.BatchNorm2d(32)
+
+    self.conv2=nn.Conv2d(32,64,kernel_size=4,stride=1, padding=0)
+    self.bn2=nn.BatchNorm2d(64)
+
+    self.conv3=nn.Conv2d(64,128,kernel_size=4,stride=1,padding=0)
+    self.bn3=nn.BatchNorm2d(128)
+
+    self.conv4=nn.Conv2d(128,128,kernel_size=4,stride=1,padding=0)
+    self.bn4=nn.BatchNorm2d(128)
+
+    self.pool=nn.MaxPool2d(kernel_size=3, stride=3)
+    self.pool2=nn.MaxPool2d(kernel_size=3,stride=2)
+
+    self.fc1=nn.Linear(6*6*128,512)
+    self.fc2=nn.Linear(512,classes)
+
+    self.flatten=nn.Flatten()
+    self.relu=nn.ReLU()
+    self.dropout=nn.Dropout(0.5)
+
+  def forward(self,x):
+    x=self.conv1(x)
+    x=self.bn1(x)
+    x=self.relu(x)
+    x=self.pool(x)
+
+    x=self.conv2(x)
+    x=self.bn2(x)
+    x=self.relu(x)
+    x=self.pool(x)
+
+    x=self.conv3(x)
+    x=self.bn3(x)
+    x=self.relu(x)
+    x=self.pool2(x)
+
+    x=self.conv4(x)
+    x=self.bn4(x)
+    x=self.relu(x)
+    x=self.flatten(x)
+
+    x=self.fc1(x)
+    x=self.relu(x)
+    x=self.dropout(x)
+
+    x=self.fc2(x)
+    return x
+
+# Load your trained PyTorch model
+model = CNNmodel(7)  # Replace with your model class
+model.load_state_dict(torch.load("best_model_large.pth", map_location=torch.device('cpu')))  # Load weights
+model.eval()  # Set to evaluation mode
+
 @app.route('/')
 def home():
     return "Welcome to the Cotton Disease Classification API!"
